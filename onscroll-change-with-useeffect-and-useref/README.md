70fc01d529a3bd2ffbc745f6493b3e856f4c15e0
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-08-11 08:16:35 -0700

    onscroll change using useState, useRef and useEffect

---
+++ onscroll-change-with-useeffect-and-useref/README.md
@@ -0,0 +1,2 @@
+# Installations
+- Using useref to change the colorization of a picture on mouse over and mouse out
---
+++ onscroll-change-with-useeffect-and-useref/src/ImageToggleOnScroll.js
@@ -0,0 +1,33 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const ImageToggleOnMouseOver = ({primaryImg, secondaryImg}) => {
+    const imageRef = useRef(null);
+
+    const isInView = () => {
+        const rect = imageRef.current.getBoundingClientRect();
+        return rect.top >= 0 && rect.bottom <= window.innerHeight;
+    }
+
+    const [inView, setInView] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener("scroll", scrollHandler);
+        return() => {
+            window.addEventListener("scroll", scrollHandler);
+        };
+    },[]);
+
+    const scrollHandler = () => {
+        setInView(isInView());
+    };
+
+    return (
+        <img 
+            src={inView ? secondaryImg : primaryImg}
+            alt=""
+            ref={imageRef}
+        />
+    );
+};
+
+export default ImageToggleOnMouseOver;
