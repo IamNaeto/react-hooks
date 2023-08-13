1824ec408b0b05fcb83c6b808d9886f34c834d84
Charles Obimnaeto Egesionu <egesionucharlesobimnaeto@gmail.com>
2023-08-13 01:34:19 -0700

    react hooks and eslint

---
+++ react-hooks-eslint-and-usage/pages/index.js
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from "react";
+
+const InputElement = () => {
+  const random_boolean = Math.random() >= 0.5;
+  const [isLoading, setIsLoading] = useState(random_boolean === true);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 2000)
+  })
+  return isLoading ? <div>Loading...</div> : <input placeholder="Enter Some Text" />;
+};
+
+export default InputElement;
